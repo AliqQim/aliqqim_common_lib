d8751f3fd8d0ee99bb9c751092fa65f9637f3c34
AliqQim <hnero@mail.ru>
2020-04-19 19:01:32 +0600

    фиксики

--- CommonUtils/FileUtils.cs
+++ CommonUtils/FileUtils.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -29,6 +30,30 @@ namespace CommonUtils
                 return null;
 
             return Path.Combine(availableParts.Reverse().ToArray());
+        }
+
+        /// <summary>
+        /// возвращает путь до папки проекта (если быть точным, до папки, в которой ~Debug\Bin\*сборка*.dll)
+        /// 
+        /// за основу взято https://sau001.wordpress.com/2019/02/24/net-core-unit-tests-how-to-deploy-files-without-using-deploymentitem/
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPathToCurrentAssemblyProjectFolder()
+        {
+            return GetPathToAssemblyProjectFolder(Assembly.GetExecutingAssembly());
+        }
+
+        public static string GetPathToAssemblyProjectFolder(Assembly assembly)
+        {
+            string pathAssembly = assembly.Location;
+            string folderAssembly = Path.GetDirectoryName(pathAssembly);
+            string binFolder = ClosestParentFolderOrNull(folderAssembly, "bin");
+            if (binFolder == null)
+                throw new Exception("Не нашлась папка 'bin'");
+            string folderProjectLevel =
+                Path.GetFullPath(Path.Combine(binFolder, "..", ".."));
+
+            return folderProjectLevel;
         }
     }
 }
--- CommonUtils/TestHelpers.cs
+++ CommonUtils/TestHelpers.cs
@@ -4,22 +4,9 @@ namespace CommonUtils
 {
     public class TestHelpers
     {
-        
-        #region https://sau001.wordpress.com/2019/02/24/net-core-unit-tests-how-to-deploy-files-without-using-deploymentitem/
-        
-        /// <summary>
-        /// Get the path to the current unit testing project.
-        /// </summary>
-        /// <returns></returns>
-        public static string GetPathToCurrentUnitTestProject()
-        {
-            string pathAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            string folderAssembly = System.IO.Path.GetDirectoryName(pathAssembly);
-            if (folderAssembly.EndsWith("\\") == false) folderAssembly = folderAssembly + "\\";
-            string folderProjectLevel = System.IO.Path.GetFullPath(folderAssembly + "..\\..\\");
-            return folderProjectLevel;
-        }
-        #endregion
+
+
+        //пока пусто
 
     }
 }
