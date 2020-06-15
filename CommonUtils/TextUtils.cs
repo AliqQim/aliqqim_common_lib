3d0c9a837f800412b5cae4b4571794537951986a
AliqQim <hnero@mail.ru>
2020-06-15 21:42:58 +0600

    добавляю StripTags

---
+++ CommonUtils/TextUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CommonUtils
+{
+    public class TextUtils
+    {
+
+        //https://stackoverflow.com/a/2334958
+        public static string StripTags(string htmlString)
+        {
+            string pattern = @"<(.|\n)*?>"; //OPTIMIZE make lazy static, compiled
+
+            return Regex.Replace(htmlString, pattern, string.Empty);
+        }
+    }
+}
